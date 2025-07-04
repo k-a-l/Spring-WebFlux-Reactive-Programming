741b85409b650ba6d619be058898a1a6488987fb
kk <kalyankatwal2@gmail.com>
2025-07-04 17:46:41 +0545

    Back Pressure

--- src/main/java/com/kalyan/reactivewebflux/handler/CustomerStreamHandler.java
+++ src/main/java/com/kalyan/reactivewebflux/handler/CustomerStreamHandler.java
@@ -7,17 +7,25 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.BufferOverflowStrategy;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerStreamHandler {
     private final CustomerDao customerDao;
 
-    public Mono<ServerResponse> getCustomerResponse(ServerRequest req){
-        Flux<CustomerDto> allStream = customerDao.findAllStream();
-        return ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM)
-                .body(allStream, CustomerDto.class);
+    public Mono<ServerResponse> getCustomerResponse(ServerRequest req) {
+        Flux<CustomerDto> throttled = customerDao.findAllList()
+                .delayElements(Duration.ofSeconds(1))
+                .onBackpressureBuffer(10, BufferOverflowStrategy.DROP_LATEST);
+
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(throttled, CustomerDto.class);
+
     }
 }
